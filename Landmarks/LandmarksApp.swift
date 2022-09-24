3c087b20bc38444dd3b81db2d3a3e79abccc01d5
Carlos eduardo <carlos.contato00@gmail.com>
2022-09-24 09:47:01 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Carlos Eduardo on 24/09/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
